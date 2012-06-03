3a7bd5f59e4f7f7fbb5e466eaef0296596047b75
Josh Borts <jborts@gmail.com>
2012-06-03 16:21:25 -0400

    Khurram is god

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,5 +37,19 @@ InvestigateNet::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
-  config.action_mailer.delivery_method = :letter_opener
+#  config.action_mailer.delivery_method = :letter_opener
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :tls => true,
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => 'invstg8.net',
+    :user_name => 'admin@invstg8.net',
+    :password => 'swkinadmin001',
+    :authentication => 'plain',
+    :enable_starttls_auto => true
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,19 @@ InvestigateNet::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  DOMAIN = "www.instvg8.net" # For now, while on heroku, for staging.
+  DOMAIN = "invstg8.net" # For now, while on heroku, for staging.
   config.action_mailer.default_url_options = { :host => DOMAIN }
-
-  config.action_mailer.delivery_method = :ses
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :tls => true,
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => 'invstg8.net',
+    :user_name => 'admin@invstg8.net',
+    :password => 'swkinadmin001',
+    :authentication => 'plain',
+    :enable_starttls_auto => true
+  }
 end
